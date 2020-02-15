12598b0ad9eaef3a5ede4a1062f18d2b37987839
ryuryu-ninja <dragon.kawana@gmail.com>
2020-02-15 12:59:39 +0900

    Mod index.js

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ let obj = {};
 obj.param1 = [1,2,3,4];
 console.log(obj);
 
+class test {
+
+	constructor(){
+		this.a = "a";
+	}
+
+	dump(){
+		console.log(this.a);		
+	}
+
+}
+
+let tobj = new test();
+tobj.dump();
 
